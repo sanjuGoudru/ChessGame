9123292ff0eb17dcf0d6c390221ff1c4985fe025
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-16 06:57:24 +0530

    Position.java: getters and setters done

--- src/main/Position.java
+++ src/main/Position.java
@@ -1,5 +1,28 @@
 package main;
 
 public class Position {
-
+	private int x,y;
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) throws IllegalArgumentException{
+		if(x>=0 && x<=7)
+			this.x = x;
+		else
+			throw new IllegalArgumentException("X value invalid. X: "+x);
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		if(y>=0 && y<=7)
+			this.y = y;
+		else
+			throw new IllegalArgumentException("y value invalid. Y: "+y);
+	}
+	
 }
