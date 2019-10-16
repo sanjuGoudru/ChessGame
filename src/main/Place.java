df080b3cb879cef9c72b833397bb75739dc77fe2
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-16 07:00:49 +0530

    Place.java constructors completed

--- src/main/Place.java
+++ src/main/Place.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Place {
-
+	private Position position;
+	private ChessPiece piece;
+	public Place(Position position,ChessPiece piece) {
+		this.position = position;
+		this.piece = piece;
+	}
+	public Place(Position position) {
+		this(position,null);
+	}
 }
