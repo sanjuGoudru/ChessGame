152b3f7fa41fe283d55f12274ec64fc07dbeac17
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-18 06:27:33 +0530

    ChessPiece basic methods implemented

--- src/main/ChessPiece.java
+++ src/main/ChessPiece.java
@@ -1,10 +1,35 @@
 package main;
 
 public class ChessPiece {
-
-	public void setAvailability(boolean b) {
-		// TODO Auto-generated method stub
-		
+	private boolean avialable;
+	private Position position;
+	
+	public ChessPiece(boolean available, Position position) {
+		this.avialable = available;
+		this.position = position;
 	}
-
+	
+	public boolean isAvailable() {
+		return avialable;
+	}
+	public void setAvailability(boolean available) {
+		this.avialable = available;
+	}
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	public boolean isValid(ChessBoard board, Position beforePos, Position afterPos){
+		int beforeX = beforePos.getX();
+		int beforeY = beforePos.getY();
+		int afterX = afterPos.getX();
+		int afterY = afterPos.getY();
+        if(beforeX == afterX || beforeY == afterY )
+            return false;
+        if(afterX < 0 || afterX > 7 || beforeX < 0 || beforeX > 7 || afterY < 0 || afterY > 7 || beforeY <0 || beforeY > 7)
+            return false;
+        return true;
+    }
 }
